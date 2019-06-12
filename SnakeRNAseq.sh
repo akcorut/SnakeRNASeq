45164ccb649ed5cf4950c46c7c57e85c81729138
Kivanc Corut <42179487+akcorut@users.noreply.github.com>
2019-06-12 09:37:02 -0400

    Add bash script

---
+++ SnakeRNAseq.sh
@@ -0,0 +1,20 @@
+#PBS -S /bin/bash
+#PBS -q wallace_q 
+#PBS -N SnakemakeRnaSeq
+#PBS -l walltime=48:00:00
+#PBS -l nodes=1:ppn=40
+#PBS -l mem=240gb
+
+#PBS -M ac32082@uga.edu
+#PBS -m ae
+#PBS -j oe
+
+cd /scratch/ac32082/PeanutRnaSeq
+
+module load Anaconda3/5.0.1
+source activate snakemake-rnaseq
+
+export LC_ALL=en_SG.utf8
+export LANG=en_SG.utf8
+
+snakemake --use-conda --cores 40 -s Snakefile.RNAseq
